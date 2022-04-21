108b9fa8210b46a841542788c74a5071005db078
Diana <84850486+diana-starz@users.noreply.github.com>
2022-04-21 06:51:34 +0200

    Add files via upload

---
+++ &&, średnia arytmetyczna, potrójny warunek.js
@@ -0,0 +1,34 @@
+/*
+    6. Losuj trzy liczby a, b, c dopoki nie zajdzie warunek a<b<c. Wyznacz srednia arytmetyczna liczb a oraz c
+       i sprawdz czy jest 2 razy wieksza od b.
+*/
+
+function drawNumber(min,max) {
+    return Math.floor(Math.random() * (max-min) +min);
+}
+
+const a = drawNumber(1,100);
+const b = drawNumber(1,100);
+const c = drawNumber(1,100);
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+function areAscendingSequence(a, b, c) {
+    return a < b && b < c;
+}
+
+
+
+function average(a, b) {
+    return (a + b) / 2;
+}
+
+function checkNumbers(a, b, c) {
+    return average(a, c) > 2 * b;
+}
+
+
+
+console.log(checkNumbers(a,b,c));
