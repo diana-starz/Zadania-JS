bb8e96795a7564b49d375cbfd1bf6e332314af01
Diana <84850486+diana-starz@users.noreply.github.com>
2022-04-23 16:02:10 +0200

    Add files via upload

---
+++ IIFE.js
@@ -0,0 +1,17 @@
+
+(function () {
+    console.log('OD RAZU SIE WYKONUJE!')
+})();
+
+const r = (function (x, y) {
+    return x + y;
+})(100, 200);
+console.log(r);
+
+
+(function () {
+    let y = 20;
+    let x =16
+    console.log(`x = ${x} y = ${y}`);
+})();
+
