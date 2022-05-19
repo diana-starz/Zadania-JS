7a43d85de0881b4cca62d5743fd5e04d50ef4a23
Diana <84850486+diana-starz@users.noreply.github.com>
2022-05-19 05:53:23 +0200

    Add files via upload

---
+++ Zadanie z tablic,reduce - KM..js
@@ -0,0 +1,29 @@
+
+const rowAvg = (numbers) => numbers.reduce((x, y) => x + y) / numbers.length;
+
+const getMaxAvgRow = (rows) => rows.reduce(
+    (res, row, idx) => {
+        const avg = rowAvg(row);
+        console.log(avg)
+
+        if (res.avg === undefined || avg > res.avg) {
+            return { idx, avg}
+        }
+        return res;
+    },
+
+    {
+        idx: undefined,
+        avg: undefined
+    });
+
+
+const main = () => {
+    const arr = [
+        [2, 1, 5, 6],
+        [2, 3, 7, 6],
+        [7, 9, 8, 6]
+    ];
+    console.log(getMaxAvgRow(arr))
+}
+main();
